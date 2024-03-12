e1f5fd965d66fdd8aa82b92557bf170d94e7915e
Rich Brown <richb.hanover@gmail.com>
2024-03-11 22:31:16 -0400

    First commit for AddTimestamp.sh that takes meeting name/date, and starting time stamp; It combines the audio with the static image, then adds the label and timecode; Output file is meeting-name-timestamped.mov

---
+++ AddTimestamp.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+#
+# Combine Image and Audio file to produce a .mov output file
+#
+# Usage: 
+# 1. Place an image file (.jpeg) and audio file (.mp4) in this folder.
+# 2. Run the ImageAndAudio.sh script. It will scan for a .jpeg file and a .mp4 file.
+# 3. Result is a .mov file with "Combined-" plus the same name as the .mp4 file
+# 4. Discard/Move the source .mp4 file out of the directory
+# 5. Move the .mov file to OutputFiles for safe keeping or later processing
+#
+# Invoke with:
+# sh ./WithTimestamp.sh "Lyme Select Board-6Feb2024" 09:58:00
+
+# Get the label text
+label="$1"
+
+# Extract hours, minutes, and seconds
+IFS=: read -r hours minutes seconds <<< "$2"
+
+# Calculate total seconds
+# start_frame=$((hours * 3600 + minutes * 60 + seconds))
+start_time="$hours\:$minutes\:$seconds\;00"
+
+echo "***** $label" "$start_frame" "$start_time"
+
+# Get the base filename of ".mp4" files in the current directory
+file_name=$(find . -maxdepth 1 -type f -name "*.mp4" -exec basename {} \;)
+
+# Append ".mov"
+outfile="${file_name%.mp4}-timestamped.mov"
+
+# Run the ffmpeg command to read the two files and produce a .mov
+#   There are lots of fussy options. See the README.md for details
+ffmpeg \
+	-r 1 \
+	-loop 1 \
+	-y  \
+	-i *.jpeg \
+	-i "${file_name}" \
+	-c:a copy \
+	-r 1 \
+	-vcodec libx264 \
+	-shortest \
+	-pix_fmt yuv444p \
+	-vf "drawtext=text=${label}:x=100:y=580:fontsize=24:fontsize=36:fontcolor=white: box=1:boxcolor=gray, \
+		drawtext=timecode='$start_time': r=30: x=700: y=580: fontsize=36:fontcolor=white: box=1:boxcolor=gray" \
+	"$outfile"
+
+	
+#	-vf "drawtext=timecode=$start_time: r=30: x=700: y=580: fontcolor=white: box=1: boxcolor=0x00000000@1" 	\
+
+	# drawtext=text=${label}:x=100:y=580:fontsize=24, 
+	# 	-vf "drawtext=text='timecode':rate=30:start_number=$start_frame:x=700:y=580:fontsize=24" \
+
+# From https://trac.ffmpeg.org/wiki/FilteringGuide#BurntinTimecode
+# ffmpeg -i in.mp4 -vf "drawtext=fontfile=/usr/share/fonts/truetype/DroidSans.ttf: timecode='09\:57\:00\;00': r=30: \
+#    x=(w-tw)/2: y=h-(2*lh): fontcolor=white: box=1: boxcolor=0x00000000@1" -an -y out.mp4
